fdd114c1d59ace04bb91d62f4e3d278310881baa
Krzysztof Januś <fluke.kuczwa@gmail.com>
2014-05-14 22:49:09 +0200

    SecurityBundle was rebuilded

---
+++ src/Tutto/SecurityBundle/Configuration/Privilege/UserNotLoggedException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Tutto\SecurityBundle\Configuration\Privilege;
+
+use Tutto\SecurityBundle\Configuration\PrivilegeException;
+
+/**
+ * @author fluke.kuczwa@gmail.com
+ */
+class UserNotLoggedException extends PrivilegeException { }
