b4bb78fcff45c9c73dea3e96ed663bde1c6d8433
Krzysztof Januś <fluke.kuczwa@gmail.com>
2014-06-16 20:20:34 +0200

    dev

---
+++ src/Tutto/CommonBundle/Tools/Status.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Tutto\CommonBundle\Tools;
+
+/**
+ * Class Status
+ * @package Tutto\CommonBundle\Tools
+ */
+class Status {
+    const ENABLED  = 1;
+    const DISABLED = 0;
+} 
---
+++ src/Tutto/FrontendBundle/Controller/ExceptionController.php
@@ -0,0 +1,11 @@
+<?php
+namespace Tutto\FrontendBundle\Controller;
+
+use Symfony\Component\HttpKernel\Log\DebugLoggerInterface;
+
+/**
+ * Class ExceptionController
+ * @package Tutto\FrontendBundle\Controller
+ */
+class ExceptionController extends \Symfony\Bundle\TwigBundle\Controller\ExceptionController {
+} 
